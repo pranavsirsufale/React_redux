9331cfc553bc56c326f7fef188044033f86c4163
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-12 16:48:41 +0530

    redux application

---
+++ redux_app/src/store/store.jsx
@@ -0,0 +1,26 @@
+const initialState = {
+    task : [],
+}
+
+const ADD_TASK = 'task/add';
+const DELETE_TASK  = 'task/delete'
+
+const taskReduder = ( state = initialState,action) => {
+    switch(action.type ){
+        case ADD_TASK:
+            return  { 
+                ...state,
+                task : [...state.task,action.payload]
+            }
+        case DELETE_TASK:
+            return { 
+                ...state,
+                task : state.task.filter((currentTask,id)=>(
+                    id !== action.payload
+                ))
+            }
+        default :
+        break;
+    }
+
+}
